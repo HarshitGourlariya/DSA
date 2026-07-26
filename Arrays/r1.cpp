5915d6bf2ab78bbe3e011a2fd7122cd8a5f56ab5
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-26 12:02:37 +0530

    Max Product of two elements

---
+++ Arrays/r1.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int max_1(int x,int y)
+{
+    if(x>y)
+    return x;
+    else
+    return y;
+}
+int Product(int arr[],int n)
+{
+    int i;
+    int max= INT_MIN , smax;
+    int min = INT_MAX , smin;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            smax = max;
+            max = arr[i];
+        }
+        else if(arr[i]>smax)
+        {
+            smax = arr[i];
+        }
+
+        if(arr[i]<min)
+        {
+            smin = min;
+            min = arr[i];
+        }
+        else if(arr[i]<smin)
+        {
+            smin = arr[i];
+        }
+
+    }
+    return max_1(max*smax,min*smin);
+}
+int main()
+{
+    int i,n;
+    cin >>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    int result = Product(arr,n);
+    cout << result;
+}
