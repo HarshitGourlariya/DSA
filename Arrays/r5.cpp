ba11d4f6091226accfbf58e392f9cc81728313a7
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-27 08:44:43 +0530

    Find the majority element n/2>>

---
+++ Arrays/r5.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int voting(int arr[],int n)
+{
+    int candidate = 0;
+    int count =0;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(count==0)
+        {
+            candidate = arr[i];
+            count =1;
+            
+        }
+        else if(arr[i]==candidate)
+        {
+            count++;
+        }
+        else
+        count--;
+    }
+
+    count =0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==candidate)
+        count++;
+        if(count>n/2)
+        return candidate;
+    }
+    return -1;
+}
+int main()
+{
+    int i,n;
+    cin >>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    int result = voting(arr,n);
+    cout << result;
+
+   
+}
