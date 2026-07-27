8a203119cfb2090336eaa7e8640e3b4572cf0cdc
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-27 08:49:40 +0530

    Count elements smaller than the current element

---
+++ Arrays/r6.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin >> n;
+    int arr[n],brr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        int count =0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            count++;
+        }
+        brr[i]=count;
+    }
+    for(i=0;i<n;i++)
+    {
+        cout << brr[i]<<" ";
+    }
+}
