bed3cdee883eb0d5d24efce6ec4a2a3679404cbf
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-30 09:03:59 +0530

    First repeating element

---
+++ Arrays/r14.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin >>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        int count =0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {cout << arr[i];
+            break;}
+        }
+    }
+}
