0e20a2548f77ad2400ee725e82f7867a0e1a4c91
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-30 09:00:04 +0530

    First non repeating element

---
+++ Arrays/r13.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin >>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        int count =0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            count++;
+        }
+        if(count==1)
+        cout << arr[i];
+    }
+}
