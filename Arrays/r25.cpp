8d83cb7b8d5542acf53c92ee449743a58859d0b8
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-03 09:11:40 +0530

    Reverse the array without using another array

---
+++ Arrays/r25.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin >>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    int left=0;
+    int right = n-1;
+    while(left<right)
+    {
+        int temp = arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
+    }
+    for(i=0;i<n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
