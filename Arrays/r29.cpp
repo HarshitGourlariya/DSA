8dbdb5ee062a329d88987a346a1d3deb18c3131a
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-04 09:12:52 +0530

    Second largest element

---
+++ Arrays/r29.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,max=0,smax;
+    cin >>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            smax = max;
+            max = arr[i];
+        }
+        else if(arr[i]>smax)
+        {
+            smax = arr[i];
+        }
+
+    }
+    cout << smax;
+
+}
