4b1eddb9fc4b33b05e855b0e6ef0843edd036027
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-04 09:35:13 +0530

    Check the given string is Palindorme or not

---
+++ Strings/r9.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check(string s)
+{
+    int left =0;
+    int right = s.length()-1;
+    while(left<right)
+    {
+        if(s[left]!=s[right])
+        return false;
+        left++;
+        right--;
+
+    }
+    return true;
+}
+int main()
+{
+    string s;
+    cin >>s;
+    int result = check(s);
+    cout << result;
+   
+}
