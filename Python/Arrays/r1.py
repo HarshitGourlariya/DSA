9d099ec33d5de8b66a8463f29a95d80bc86a9bd6
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-06 09:21:30 +0530

    Second largest number in Python

---
+++ Python/Arrays/r1.py
@@ -0,0 +1,11 @@
+arr = list(map(int,input().split()))
+max = 0;
+
+for i in range(len(arr)):
+    if(arr[i]>max):
+        smax = max;
+        max = arr[i];
+    elif(arr[i]>smax):
+        smax = arr[i];
+    
+print(smax)
