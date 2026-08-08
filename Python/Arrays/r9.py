2c88b0452c9f8d5287f3647c8c603b53c79e14fd
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-08 09:16:00 +0530

    third largest element

---
+++ Python/Arrays/r9.py
@@ -0,0 +1,17 @@
+arr = list(map(int,input().split()))
+min = float('inf')
+max = float('-inf')
+smax = float('-inf')
+smin = float('inf')
+for i in range(len(arr)):
+    if(arr[i]>max):
+        tmax =smax
+        smax = max
+        max = arr[i]
+    elif(arr[i]>smax & smax<max):
+        tmax = smax
+        smax = arr[i]
+    else:
+        tmax = arr[i]
+        
+print(tmax)
