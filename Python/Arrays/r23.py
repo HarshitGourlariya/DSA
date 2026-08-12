49a081c5fe3199e367a08be6287a2e403b174c97
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-12 11:37:49 +0530

    Count pairs of the given sum

---
+++ Python/Arrays/r23.py
@@ -0,0 +1,10 @@
+sum = int(input())
+arr = list(map(int,input().split()))
+count =0
+for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+        if(arr[i]+arr[j]==sum):
+            count+=1;
+
+print(count)
+    
