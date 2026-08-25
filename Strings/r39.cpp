e6d6d74f3906bd1446ed495a59c16a7bf338cd44
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-25 10:04:20 +0530

    Rev

---
+++ Strings/r39.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >>s;
+    int idx=1;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]!=s[idx-1])
+        {
+            s[idx]=s[i];
+            idx++;
+        }
+
+    }
+    for(int i=0;i<idx;i++)
+    {
+        cout << s[i];
+    }
+}
